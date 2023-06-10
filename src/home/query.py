4a903e61b8a44eddc6f83396fc634db0e7abc6c3
ardhanidzaky <82014329+ardhanidzaky@users.noreply.github.com>
2023-06-10 11:20:55 +0700

    Edit influencers query

--- src/home/query.py
+++ src/home/query.py
@@ -19,17 +19,61 @@ def get_all_services_query() -> str:
 
 def get_all_influencers_query() -> str:
     QUERY = """
+    WITH
+        user_have_instagram AS (
+            SELECT  user_id
+            FROM    socmed_account
+            WHERE   social_media = 'Instagram'
+        )
+
+        , user_have_twitter AS (
+            SELECT  user_id
+            FROM    socmed_account
+            WHERE   social_media = 'Twitter'
+        )
+
+        , user_have_tiktok AS (
+            SELECT  user_id
+            FROM    socmed_account
+            WHERE   social_media = 'Tiktok'
+        )
+
     SELECT  "user".id AS user_id
             , "user".username AS user_name
             , "user".email AS user_email
             , city.city AS user_city
             , field_area.type AS user_field_area
+            , CASE
+                WHEN "user".id IN (SELECT user_id FROM user_have_instagram)
+                THEN (  SELECT  followers
+                        FROM    socmed_account
+                        WHERE   user_id = "user".id
+                                AND social_media = 'Instagram')
+                ELSE 0
+            END AS num_followers_instagram
+            , CASE
+                WHEN "user".id IN (SELECT user_id FROM user_have_twitter)
+                THEN (  SELECT  followers
+                        FROM    socmed_account
+                        WHERE   user_id = "user".id
+                                AND social_media = 'Twitter')
+                ELSE 0
+            END AS num_followers_twitter
+            , CASE
+                WHEN "user".id IN (SELECT user_id FROM user_have_tiktok)
+                THEN (  SELECT  followers
+                        FROM    socmed_account
+                        WHERE   user_id = "user".id
+                                AND social_media = 'Tiktok')
+                ELSE 0
+            END AS num_followers_tiktok
     FROM    "user"
     LEFT JOIN   city
     ON  "user".city_id = city.id
     LEFT JOIN   field_area
     ON  "user".field_area_id = field_area.id
-    WHERE   "user".tipe = 'Influencer';
+    WHERE   "user".tipe = 'Influencer'
+            AND count <= 20;
     """
     
     return QUERY
